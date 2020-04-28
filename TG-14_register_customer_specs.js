a44975a6f3176d0a5838f0adccddf12bf59cb7a5
nik-gus <niklas.gustafsson@gmail.com>
2020-04-29 00:33:56 +0200

    TG-14 updated for SUT 2.11.0

--- TG-14_register_customer_specs.js
+++ TG-14_register_customer_specs.js
@@ -1,13 +1,21 @@
+/**
+ * Author: Niklas
+ */
+
 describe('TG-14 Som en kund vill jag kunna registrera mig', () => {
     
     before(() => {
         cy.visit('/')
-
-        //select My Account
-        cy.get('li.click_menu').should('contain.text', 'My Account').click()
-
-        //selct Register
-        cy.get('#registerLink').should('have.text', 'Register').click()
+        cy
+            //open MyAccount dropdown
+            .get('#customerAccount > .dropdown-toggle')
+            .should('contain.text', 'My Account')
+            .trigger('mouseover', {force:true})
+        cy
+            //selct Register
+            .get('#registerLink')
+            .should('have.text', 'Register')
+            .click()
     })
 
     beforeEach(() => {
@@ -16,8 +24,8 @@ describe('TG-14 Som en kund vill jag kunna registrera mig', () => {
         //fill out registration form (source: support/commands.js)
         cy.fillAndVerifyRegistrationForm()
     })
-
-    it('greets you with "Personal information"', () => {
+    
+    it('greets with "Personal information"', () => {
         cy.contains('h3', 'Personal information')
     })
 
@@ -26,12 +34,15 @@ describe('TG-14 Som en kund vill jag kunna registrera mig', () => {
              //submit form with blank first name field
             .get('#firstName')
             .clear()
-            .get('form').eq(1).submit()
-
+            .get('form').eq(2).submit()
         cy
-            //verify error message
-            .get('.error')
-            .should('have.text', 'Field required')
+            //verify registration rejected
+            .location('pathname')
+            .should('not.include', '/dashboard')
+        cy
+            //verify error message exists
+            .get('#customer\\.errors')
+            .should('exist')
     })
 
     it('requires email address', () => {
@@ -39,13 +50,13 @@ describe('TG-14 Som en kund vill jag kunna registrera mig', () => {
             //submit form with bland email field
             .get('#emailAddress')
             .clear()
-            .get('form').eq(1)
+            .get('form').eq(2)
             .submit()
-
         cy
             //verify error message
-            .get('.error')
-            .should('have.text', 'Email address is required')
+            .get('#customer\\.errors')
+            .should('exist')
+            .and('have.text', 'Unable to complete registration, please try again later')
     })
 
     it('rejects invalid email address', () => {
@@ -53,13 +64,17 @@ describe('TG-14 Som en kund vill jag kunna registrera mig', () => {
             //submit form with invalid email
             .get('#emailAddress')
             .clear()
-            .type('badFormedEmailAddress.com{enter}')
-
+            .type('badFoormedEmail.com{enter}')
+        cy
+            //verify registration rejected
+            .location('pathname')
+            .should('not.include', '/dashboard')    
         cy
             //verifies error message
-            .get('.error')
+            .get('#registrationError')
             .should('exist')
-            .and('have.text', 'Email address is invalid')
+            .and('have.text', 'Please provide a valid email address.')
+   
     })
 
     it('requires both passwords to match', () => {
@@ -68,7 +83,6 @@ describe('TG-14 Som en kund vill jag kunna registrera mig', () => {
             .get('#password')
             .clear()
             .type(Cypress.env('password')+'.')
-
         cy  
             //verify error message
             .contains('Both password must match')
@@ -80,28 +94,25 @@ describe('TG-14 Som en kund vill jag kunna registrera mig', () => {
             .get('#password')
             .clear()
             .type('AbC12')
-
         cy    
             //type the same 5 char password in second field
             .get('#passwordAgain')
             .clear()
             .type('AbC12')
-
+        cy
             //verify error message
-            cy.contains('Password must be at least 6 characters')
+            .contains('Password must be at least 6 characters')
     })
 
     it('successfully registers new user', () => {
         cy
             //sbumit form
-            .get('form').eq(1).
+            .get('form').eq(2).
             submit()
-
         cy
             //verfies registation success
             .location('pathname')
             .should('eq', '/shop/customer/dashboard.html')   
     })
-
     
 }) //end describe
